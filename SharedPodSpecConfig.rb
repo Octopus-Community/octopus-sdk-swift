ac29614ac7ed8b0ee347950c7bdc2f0855d328a9
djavan-bertrand <djavan-bertrand@users.noreply.github.com>
2025-07-30 16:27:08 +0200

    Fixed tag name in Podspecs (#19)

--- SharedPodSpecConfig.rb
+++ SharedPodSpecConfig.rb
@@ -1,7 +1,7 @@
 module SharedPodSpecConfig
   VERSION = '1.5.1'
   GITHUB_PAGE = 'https://github.com/Octopus-Community/octopus-sdk-swift'
-  SOURCE = { :git => "#{GITHUB_PAGE}.git", :tag => VERSION }
+  SOURCE = { :git => "#{GITHUB_PAGE}.git", :tag => "v#{VERSION}" }
   LICENSE = { :file => 'LICENSE.md' }
   AUTHOR = { 'Djavan Bertrand' => 'djavan.bertrand@octopuscommunity.com' }
   IOS_DEPLOYMENT_TARGET = '13.0'
